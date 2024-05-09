21332d466d24df9426bb7003b386da02b22d2a67
Ayushmak02 <83110177+Ayushmak02@users.noreply.github.com>
2024-05-10 01:48:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,22 @@ dl_model = tf.keras.models.load_model(cnn_path)
 def main():
     st.title('ADHD Detection')
     st.image(imgpath, width=300)
+    page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("https://i.postimg.cc/4xgNnkfX/Untitled-design.png");
+background-size: cover;
+background-position: center center;
+background-repeat: no-repeat;
+background-attachment: local;
+}}
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+</style>
+"""
 
+    st.markdown(page_bg_img, unsafe_allow_html=True)
     st.markdown("""
         <style>
         .big-font {
